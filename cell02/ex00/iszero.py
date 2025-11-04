4fc4ba700e91b89a60f424c9a75764e6c2382b7f
cjumpale <68160008@kmitl.ac.th>
2025-11-04 10:38:26 +0000

    msg

--- cell02/ex00/iszero.py
+++ cell02/ex00/iszero.py
@@ -1,5 +1,8 @@
-number = float(input())
-if number == 0:
-    print("This number is equal to zero")
-else:
-    print("This number is different from zero")
+try:
+    number = float(input(""))
+    if number == 0:
+        print("This number is equal to zero.")
+    else:
+        print("This number is different from zero.")
+except ValueError:
+    print("Invalid input: Please enter a valid number.")
