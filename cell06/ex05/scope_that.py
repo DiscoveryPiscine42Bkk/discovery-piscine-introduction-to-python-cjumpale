0e37e8f269382208aa2f7a2da6fa50daec8d706d
cjumpale <68160008@kmitl.ac.th>
2025-11-06 02:57:27 +0000

    comment

---
+++ cell06/ex05/scope_that.py
@@ -0,0 +1,10 @@
+def add_one(x):
+    x = x + 1
+    print("Inside the method:", x)
+
+number = 5
+print("Before calling the method:", number)
+
+add_one(number)
+
+print("After calling the method:", number)
